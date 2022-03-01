e82adbf6048b1449a0b4f8f8e6a257acedecaa61
Toehrlein27 <kaptaunkrunch27@gmail.com>
2022-02-28 19:53:25 -0700

    I mitigated the ERROR01-J bug and the FIO16-J bug in this mitigation

--- src/CSC245_Project2.java
+++ src/CSC245_Project2.java
@@ -1,37 +1,41 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+//Tim Oehrlein
+//Secure Software Development
+//Program to identify and mitigate several possible errors
+
+import java.io.*;       //Importing all the java.io classes
 
 public class CSC245_Project2 {
 
     public static void main(String[] args) {
         // Read the filename from the command line argument
-        String filename = args[0];
-        BufferedReader inputStream = null;
+        String fileName = args[0];                 //assigning fileName to argument 0
+        BufferedReader inputStream;         //buffered reader class
+        File file;                          //File class
+        String fileLine;        //String variable to read through the file
 
-        String fileLine;
         try {
-            inputStream = new BufferedReader(new FileReader(filename));
 
-            System.out.println("Email Addresses:");
-            // Read one Line using BufferedReader
-            while ((fileLine = inputStream.readLine()) != null) {
+            file = new File(System.getenv("c:\\Users\\kapta\\IdeaProjects\\CSC245_Project2_Secure") + fileName).getCanonicalFile();
+            inputStream = new BufferedReader(new FileReader(fileName));
+
+            if (!file.getPath().startsWith("c:\\Users")) {                  //if the file doesn't start with a specific string access is denied
+                System.out.println("Access denied");
+                return;
+            }
+
+            while ((fileLine = inputStream.readLine()) != null) {       //loop to read through the file selected
                 System.out.println(fileLine);
-            }
-        } catch (IOException io) {
-            System.out.println("File IO exception" + io.getMessage());
-        } finally {
-            // Need another catch for closing
-            // the streams
-            try {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-            } catch (IOException io) {
-                System.out.println("Issue closing the Files" + io.getMessage());
+
             }
 
+        } catch (IOException x) {
+            System.out.println("Invalid File");
         }
+//        try {
+//            FileInputStream fileInputStream = new FileInputStream(file);
+//        } catch (FileNotFoundException x) {
+//            System.out.println("Invalid file");
+//        }
     }
 
 }
