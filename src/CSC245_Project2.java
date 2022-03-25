b298bcb22f13dd862c351f86d4f8435b8972dfbe
Arielle <ariellcollinsb@gmail.com>
2022-03-25 10:59:12 -0600

    Header comments included to provide proper in-code attribution. inline comments included to show appropriate code changes.

--- src/CSC245_Project2.java
+++ src/CSC245_Project2.java
@@ -1,17 +1,31 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.Normalizer;
+///////////////////////////////////////////////////////////////////////////////
+//
+// Main Class File: CSC245_Project2.java
+// Semester:        Spring 2022
+// Authors:         Arielle Collins, Morgan Otto, Rylan Peterson, Tim Oehrlein
+//
+///////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader; // needed to read text from character-input stream
+import java.io.File; // needed to access file
+import java.io.FileReader; // needed to read file
+import java.io.IOException; // needed for exception handling
+import java.text.Normalizer; // needed to normalize input
 
 public class CSC245_Project2 {
-    // Read the filename from the command line argument
+    /**
+     * parses through a file and returns the contents after it is sanitized
+     * @param args - required by Java but not used
+     * @throws Exception - IO exception
+     * Canonicalize - cleans up the path by removing and resolving (., ..) etc. and returning a unique absolute path
+     * Validation - the process of checking user input or the values from the database against specific constraints
+     * Normalize - transform Unicode characters into an equivalent form, allowing for easier sorting and searching of text.
+     * Sanitize - prevents the insertion of malicious data into a subsystem
+     * Data Leakage - the unauthorized transmission of data to an external recipient
+     */
     public static void main(String[] args) throws Exception {
-
-        String filename = System.getenv("FILE_PATH");
-        if (filename == null) {
-            //exception message written so it can be read and understood by any user
-            throw new Exception("Environment file not found or is in an incorrect format.");
-        }
+        // Get canonical path from value that is configured in the FILE_PATH env variable
+        String filename = new File(System.getenv("FILE_PATH")).getCanonicalPath();
         BufferedReader inputStream = null;
 
         String fileLine;
@@ -28,7 +42,8 @@ public class CSC245_Project2 {
                 StringBuilder line = new StringBuilder(fileLine.length());
                 for(int i = 0; i < fileLine.length(); i++) {
                     char x = fileLine.charAt(i);
-                    if(Character.isLetterOrDigit(x) || x == ' ' || x == '\'') {
+                    // Only allow characters, digits, @ and .
+                    if(Character.isLetterOrDigit(x) || x == '@' || x == '.') {
                         line.append(x);
                     }
                 }
@@ -39,9 +54,8 @@ public class CSC245_Project2 {
             //stacktrace removed from system.out.println to prevent data leakage
         } catch (IOException io) {
             System.out.println("Error reading file" + filename);
+            //'Finally' block used to close the input stream
         } finally {
-            // Need another catch for closing
-            // the streams
             try {
                 if (inputStream != null) {
                     inputStream.close();
